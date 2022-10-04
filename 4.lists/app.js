6000e3256e3226ee27896bfb3f3af23c646b7b27
ElvisM89 <80644724+ElvisMasiya89@users.noreply.github.com>
2022-10-04 21:41:53 +0200

    Vue lifecycle
    Vue lifecycle

--- 4.lists/app.js
+++ 4.lists/app.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      enteredGoalValue:'',
+      goals: [] 
+    
+    };
   },
+
+  methods:{
+    addGoal(){
+      this.goals.push(this.enteredGoalValue)
+    },
+
+    removeGoal(idx){
+      this.goals.splice(idx, 1)
+    }
+  }
 });
 
 app.mount('#user-goals');
